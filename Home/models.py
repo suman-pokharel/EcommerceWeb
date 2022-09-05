bdc8115a5698866cfa56881454eb8292038e6e24
suman pokharel <sumanpokharel8@gmail.com>
2022-09-05 23:27:45 +0545

    initial commiit

---
+++ Home/models.py
@@ -0,0 +1,85 @@
+from distutils.command.upload import upload
+from email.mime import image
+from telnetlib import STATUS
+from tkinter import CASCADE
+from unicodedata import category, name
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    slug=models.TextField(unique=True)
+       
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name=models.CharField(max_length=100)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    slug=models.TextField(unique=True)
+       
+    def __str__(self):
+        return self.name
+
+STATUS=(('active','Active'),('','Default'))
+class Slider(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to ='media')
+    text=models.TextField(blank=True)
+    rank=models.IntegerField()
+    status=models.CharField(choices=STATUS,blank=True,max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to ='media')
+    rank=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+LABELS=(('new','New'),('hot','Hot'),('sale','Sale'),('','default'))
+STOCK=(('In stock','In Stock'),('out of stock','Out of Stock'))
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='media')
+    price=models.FloatField()
+    discounted_price=models.FloatField(default=0.0)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    SubCategory=models.ForeignKey(SubCategory,on_delete=models.CASCADE)
+    description=models.TextField()
+    information=models.TextField()
+    slug=models.TextField(unique=True)
+    labels=models.CharField(choices=LABELS,max_length=200)
+    stock=models.CharField(choices=STOCK,max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    review=models.TextField(blank=True)
+    date=models.CharField(max_length=100)
+    slug=models.TextField()
+    point=models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    Name=models.CharField(max_length=300)
+    email=models.EmailField(max_length=200)
+    message=models.TextField()
+
+
+
+    def __str__(self):
+      return self.Name
+
---
+++ Home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from email.message import Message
+from itertools import count
+from pickle import NONE
+from urllib import request
+from django import views
+from django.forms import SlugField
+from django.views import View
+from .models import *
+from django.views.generic import View
+import datetime,random
+from django.contrib.auth.models import User
+from django.contrib import messages,auth
+
+
+# Create your views here.
+class Base(View):
+	views = {}
+	views['categories'] = Category.objects.all()
+	views['brands'] = Brand.objects.all()
+	
+class HomeView(Base):
+    def get(self,request):
+        self.views['catagories']=Slider.objects.all()
+        self.views['news']=Product.objects.filter(labels='new')
+        self.views['hots']=Product.objects.filter(labels='hot')
+        self.views['sales']=Product.objects.filter(labels='sale')
+        return render(request, 'shop-index.html', self.views)
